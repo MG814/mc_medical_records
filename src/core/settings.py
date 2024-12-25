79080571242595e8cc34f28e5f16c1a976b12ded
MICHAŁ <mg814@wp.pl>
2024-12-25 18:12:09 +0100

    ci: updated workflows and added new libraries

--- src/core/settings.py
+++ src/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^o@ht$sh@!d43hrai^!#(#9zwbqcml&6f9)rcoll!1gr+c_&s)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions'
 ]
+
+INSTALLED_EXTENSION = [
+    'medical_records',
+]
+
+INSTALLED_APPS += INSTALLED_EXTENSION
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
