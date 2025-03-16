684e391ce879672e4c0b2702814372afb55bb034
MICHAŁ <mg814@wp.pl>
2025-03-16 16:20:22 +0100

    refactor: updated requirements.txt

--- src/core/settings.py
+++ src/core/settings.py
@@ -141,3 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+HOST = 'web-accounts'
+PORT = '8100'
+HTTP_URL = f'http://{HOST}:{PORT}'
